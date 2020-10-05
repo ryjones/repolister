bd382c7177f86f053ea9758319da723a6faea530
Ry Jones <ry@linux.com>
2020-10-05 14:21:03 -0700

    Add TWGC
    Signed-off-by: Ry Jones <ry@linux.com>

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ set -ex
 ./repo_list hyperledger-archives
 ./repo_list hyperledger-labs
 ./repo_list hyperledger-labs-archives
+./repo_list hyperledger-twgc
 find . -type f -size 0 -delete
 RC=`git diff HEAD hyperledger-*repos*.json | wc -l | awk '{print $1}'`
 if [ "$RC" != "0" ]
