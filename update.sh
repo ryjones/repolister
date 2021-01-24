1934ac4459ac0ec85fcda0386b10bd112f4a5402
Ry Jones <ry@linux.com>
2021-01-24 08:16:58 -0800

    Add branch filter
    Signed-off-by: Ry Jones <ry@linux.com>

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 ./repo_list hyperledger
-./repo_list hyperledger-archives
 ./repo_list hyperledger-labs
 ./repo_list hyperledger-labs-archives
+./repo_list hyperledger-archives
 ./repo_list hyperledger-twgc
 find . -type f -size 0 -delete
 RC=`git diff HEAD hyperledger-*repos*.json | wc -l | awk '{print $1}'`
